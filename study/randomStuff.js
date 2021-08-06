a1e671148f09d03e312396711ee9fb54f7e1fd50
akyyevtm <akyyevtm@gmail.com>
2021-08-06 13:32:38 -0400

    changing request to have meaningfull words

--- study/randomStuff.js
+++ study/randomStuff.js
@@ -1,7 +1,7 @@
 
 import { createDom } from './helper.js';
 
-const randomWord = 'http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+const randomWord = 'http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=8000&minLength=2&maxLength=15&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
 let giphyAPI = `https://api.giphy.com/v1/gifs/search?api_key=gULhDRsT8EXi5iku09zG7ndGOC61jQfH&q=`;
 
 let word = '';
