019fb8438231bab3f71cdb61d1e4e78b9b23d3ab
akyyevtm <akyyevtm@gmail.com>
2021-08-20 22:10:18 -0400

    Removed page reloading

--- study2/dict.js
+++ study2/dict.js
@@ -1,23 +1,31 @@
-import { createDom, acar } from './utils/helper.js';
+import { acar } from './utils/helper.js';
 let giphyAPI = `https://api.giphy.com/v1/gifs/search?api_key=${acar.giphy}&q=`;
 
-export async function init() {
+async function init() {
 
-    const word = await fetch('https://random-word-form.herokuapp.com/random/noun?count=1').then(res => res.json());
+    const word = await fetch('https://random-word-form.herokuapp.com/random/adjective?count=1').then(res => res.json());
     const meaning = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + word[0]).then(res => res.json());
 
-    const response2 = await fetch(giphyAPI + word[0]);
-    const picture = await response2.json();
+    const giphyResp = await fetch(giphyAPI + word[0]);
+    const picture = await giphyResp.json();
 
     const gifImage = picture?.data[0]?.images['fixed_height']?.url;
     const definition = meaning[0]?.meanings[0]?.definitions[0]?.definition;
     const audioUrl = meaning[0]?.phonetics[0]?.audio;
 
-    $('#word').text(word[0]);
-    $('#gif_image').attr('src', gifImage);
-    $('#definition').text(definition);
+    document.getElementById('word').innerHTML = word[0];
+    document.getElementById('gif_image').setAttribute('src', gifImage);
+    document.getElementById('definition').innerHTML = definition? definition : 'Couldn\'t find definition';
+    document.getElementsByTagName('audio')[0].src = audioUrl;
 
-    createDom(`<div class='audio-container'><audio controls autoplay> <source src="${audioUrl}" type="audio/mp3"></audio> </div>`);
+    // Jquery way of interacting with DOM elements
+    // $('#word').text(word[0]);
+    // $('#gif_image').attr('src', gifImage);
+    // $('#definition').text(definition);
+    // $('source').attr('src', audioUrl);
+
 }
 
 init();
+
+document.getElementById("randomWord").onclick = init;
--- study2/utils/helper.js
+++ study2/utils/helper.js
@@ -1,17 +1,3 @@
-export function createDom(element, idValue) {
-    //Create the element using the createElement method.
-    var myDiv = document.createElement("div");
-
-    //Set its unique ID.
-    myDiv.id = idValue;
-
-    //Add your content to the DIV
-    myDiv.innerHTML = element;
-
-    //Finally, append the element to the HTML body
-    document.body.appendChild(myDiv);
-}
-
 export const acar = {
     wordnik: "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5",
     giphy: "gULhDRsT8EXi5iku09zG7ndGOC61jQfH"
